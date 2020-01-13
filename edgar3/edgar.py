44bfe6456f12b37529bddd2867bbe2dd97e37645
Ken Farr <ken.farr.0@gmail.com>
2020-01-12 19:17:52 -0800

    added get_13f_listings

--- edgar3/edgar.py
+++ edgar3/edgar.py
@@ -1,2 +1,15 @@
-class edgar:
-    pass
+import datetime
+import pandas as pd
+import numpy as np
+from edgar3 import edgar_index
+
+
+def get_13f_listings(date: datetime.datetime, populate: bool) -> pd.DataFrame:
+    ed_i = edgar_index.edgar_index()
+    df = ed_i.get_full_listing_as_pd(date)
+    df = df[df["Form Type"].isin(["13F-HR", "13F-HR/A", "13F-NT", "13F-NT/A"])]
+    df["File"] = np.nan
+    if populate:
+        for index, row in df.iterrows():
+            df["File"] = ed_i.get_filing(row["File Name"])
+    return df
